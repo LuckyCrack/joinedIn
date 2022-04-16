4bc9450dd5ec4f3a05095db9629df3232fe307d9
LuckyCrack <vaibhavnanda2000@gmail.com>
2022-04-16 10:50:55 +0530

    Created Header

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Header.css'
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import WorkRoundedIcon from '@mui/icons-material/WorkRounded';
+import HeaderOption from './HeaderOption'
+
+function header() {
+  return (
+    <div className='header'>
+      <div className="header_left">
+        <img src="https://img.icons8.com/fluency/48/000000/query-inner-join.png"/>
+        <div className="header_search">
+          <input type="text" className="search" placeholder="Search" />
+          <SearchRoundedIcon />
+        </div>
+      </div>
+      
+      <div className="header_right">
+        <HeaderOption Icon={HomeRoundedIcon} title='Home'/>
+        <HeaderOption Icon={WorkRoundedIcon} title='Jobs'/>
+        <HeaderOption avatar='https://img.icons8.com/material-outlined/48/000000/user-male-circle.png' title='User Name'/>
+      </div>
+    </div>
+  )
+}
+
+export default header
